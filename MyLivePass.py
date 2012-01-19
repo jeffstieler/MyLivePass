4e24296d377214fa3168fdcca3de098a7bcd2f45
Jeff Stieler <jeff.m.stieler@gmail.com>
2012-01-19 01:01:12 -0500

    Initial commit. Logging in a LivePass user currently works, only outputs respose.

---
+++ MyLivePass.py
@@ -0,0 +1,73 @@
+import uuid
+import urllib2
+import gzip
+import StringIO
+import sys
+
+class MyLivePass(object):
+
+	def __init__(self, server=None, username=None, password=None):
+		self.cookies = {}
+		self.sessionID = None
+		self.accessCode = None
+		self.server, self.username, self.password = server, username, password
+		if self.server and self.username and self.password:
+			pass
+
+	def login(self):
+		requestBody = """
+<s:Envelope xmlns:s="http://www.w3.org/2003/05/soap-envelope" xmlns:a="http://www.w3.org/2005/08/addressing" xmlns:ch="http://schemas.compassion.com/common/headers/2005-04-05">
+	<s:Header>
+		<a:Action s:mustUnderstand="1">http://RTP.LivePass.Authentication/ILivePassAuthenticationService/Authenticate</a:Action>
+		<a:MessageID>urn:uuid:%s</a:MessageID>
+		<a:ReplyTo>
+			<a:Address>http://www.w3.org/2005/08/addressing/anonymous</a:Address>
+		</a:ReplyTo>
+		<a:To s:mustUnderstand="1">%sLivePassAuthenticationService.svc</a:To>
+		<ch:UICulture>en_US</ch:UICulture>
+	</s:Header>
+	<s:Body>
+		<Authenticate xmlns="http://RTP.LivePass.Authentication">
+			<userName>%s</userName>
+			<password>%s</password>
+			<sessionCulture>en-US</sessionCulture>
+		</Authenticate>
+	</s:Body>
+</s:Envelope>
+		""" % (uuid.uuid1(), self.server, self.username, self.password)
+
+		response = self.request("LivePassAuthenticationService.svc", requestBody)
+		print response
+
+	def request(self, path, requestBody):
+		headers = {
+			"User-Agent": "Park City 2.0 (iPhone; iPhone OS 5.0.1; en_US)",
+			"RTP-Session-Type": "Cache",
+			"Content-Type": "application/x-gzip"
+		}
+		url = self.server + path
+		data = self.compress(requestBody)
+		request = urllib2.Request(url, data, headers)
+		stream = urllib2.urlopen(request)
+		response = stream.read()
+		return self.decompress(response)
+
+	# trick using "duck typing" to get around gzip's lack of a string compression method
+	# StringIO provides a file-like object with a string storage mechanism
+	def compress(self, data):
+		s = StringIO.StringIO()
+		f = gzip.GzipFile(fileobj=s, mode="w", compresslevel=9)
+		f.write(data)
+		f.close()
+		return s.getvalue()
+
+	def decompress(self, data):
+		s = StringIO.StringIO(data)
+		f = gzip.GzipFile(fileobj=s, mode="r", compresslevel=9)
+		decompressed = f.read()
+		f.close()
+		return decompressed
+
+if __name__ == "__main__":
+	myLivePass = MyLivePass("https://secure.parkcitymountain.com/mobile/", sys.argv[1], sys.argv[2])
+	myLivePass.login()
